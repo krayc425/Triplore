df14d62927ab86711ce1c2eb3c3608f4bce6e11e
141210026 <141210026@smail.nju.edu.cn>
2017-05-23 18:07:55 +0800

    try xunique

---
+++ Triplore/Triplore/Utilities.m
@@ -0,0 +1,18 @@
+//
+//  Utilities.m
+//  Triplore
+//
+//  Created by 宋 奎熹 on 2017/5/23.
+//  Copyright © 2017年 宋 奎熹. All rights reserved.
+//
+
+#import "Utilities.h"
+#import <UIKit/UIKit.h>
+
+@implementation Utilities
+
++ (UIColor *)getColor{
+    return [UIColor colorWithRed:0.0 green:204.0/255.0 blue:204.0/255.0 alpha:1.0];
+}
+
+@end
